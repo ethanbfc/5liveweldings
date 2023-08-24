35ddbcc059d64c7614cf946138a357763c365031
ethanbfc <ethanbfc@gmail.com>
2023-08-24 14:40:55 +0100

    feat: Pages for individual products

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    path('steel-gates/', SteelGatesView.as_view(), name="steel-gates"),
+    path('composite-gates/', CompositeGatesView.as_view(), name="composite-gates"),
+    path('railings/', RailingsView.as_view(), name="railings"),
+    path('security/', SecurityView.as_view(), name="security"),
+    path('steel-framed-treated-timber-infill/', SteelFramedTreatedTimberInfillView.as_view(), name="steel-framed-treated-timber-infill"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,18 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+class SteelGatesView(TemplateView):
+    template_name = 'steel_gates.html'
+
+class CompositeGatesView(TemplateView):
+    template_name = 'composite_gates.html'
+
+class RailingsView(TemplateView):
+    template_name = 'railings.html'
+
+class SecurityView(TemplateView):
+    template_name = 'security.html'
+
+class SteelFramedTreatedTimberInfillView(TemplateView):
+    template_name = 'steel_framed_treated_timber_infill.html'
